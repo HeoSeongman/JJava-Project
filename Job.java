e7f2d9ffdde126d3f72c05ec3cf516bc4a59442b
HeoSeongman <tjdaks0804@gmail.com>
2023-02-28 15:43:44 +0900

    PC Commit 3

---
+++ Job.java
@@ -0,0 +1,15 @@
+public class Job {
+    int aaa;
+    int bbb;
+    int ccc;
+
+    public Job() {
+	this.aaa = 5;
+	this.bbb = 6;
+	this.ccc = 7;
+    }
+
+    public String toString() {
+	return this.aaa + " === " + this.bbb + " === " + this.ccc + " !@# ";
+    }
+}
